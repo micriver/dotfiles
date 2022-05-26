fd9c34f8492dc826b72f07682d69c583c0f99255
Michael Rivera <mrivera.dir@gmail.com>
2022-05-26 11:54:23 -0400

    'simplified entering commit messages because Im lazy'

--- gk-gitpush-2022.sh
+++ gk-gitpush-2022.sh
@@ -2,6 +2,8 @@
 
 # NOTES:
 # checking num args: https://stackoverflow.com/questions/18568706/check-number-of-arguments-passed-to-a-bash-script
+# check if files exist: https://linuxize.com/post/bash-check-if-file-exists/
+# check for args submission: https://unix.stackexchange.com/questions/25945/how-to-check-if-there-are-no-parameters-provided-to-a-command
 
 black=`tput setaf 0`
 mint=`tput setaf 10`
@@ -15,45 +17,47 @@ NC=`tput sgr0`
 
 PACKAGE=~/Documents/projects/GK-CMS-3.0/package-lock.json
 YARN=~/Documents/projects/GK-CMS-3.0/yarn.lock.json
-# if num args is > 1, the args are specific files to add then ask for commit message to assign to arg
-# if [[ "$#" -gt 1 ]]; then
+
+# check for args 
 if [ $# -eq 0 ]; then
-    echo "No arguments provided"
+    echo "Please provide a commit message"
     exit 1
 fi
+
+str="'$*'"
 #     git add "$@"
 #     read -p "Please provide a commit message: " commitMessage
 #     git commit -m "${commitMessage}"
-#     read -p "Are you using the default origin and route? [y/n] " res
-#     if 	[ "$res" = "y" ] || [ "$res" = "Y" ]; then
-#         git push origin master;
-#         git show
-#     else
-#         read -p "What is the origin and the route? " res
-#         git push "$res";
-#         git show
-#     fi
+    # read -p "Are you using the default origin and route? [y/n] " res
+    # if 	[ "$res" = "y" ] || [ "$res" = "Y" ]; then
+    #     git push origin master;
+    #     git show
+    # else
+    #     read -p "What is the origin and the route? " res
+    #     git push "$res";
+    #     git show
+    # fi
 # else
     # If there is one more arg after the commit message, then that is treated as the branch for the repo
-    if [[ "$2" ]]; then
-        # sync the package-lock file with your yarn file for every push 
-        echo "You're pushing two files"
-        if [[ -f "$PACKAGE" && -f "$YARN" ]]; then
-            echo "$PACKAGE and $YARN files exist."
-            synp --source-file yarn.lock
-        fi
-        git add .;
-        git commit -m "$1";
-        git push --set-upstream origin $2;
-        git show
-    else
+    # if [[ "$2" ]]; then
+    #     # sync the package-lock file with your yarn file for every push 
+    #     echo "You've provided a commit message and a branch path"
+    #     if [[ -f "$PACKAGE" && -f "$YARN" ]]; then
+    #         echo "$PACKAGE and $YARN files exist."
+    #         synp --source-file yarn.lock
+    #     fi
+    #     git add .;
+    #     git commit -m "$1";
+    #     git push --set-upstream origin $2;
+    #     git show
+    # else
     # if num args is just one, that's a commit message
         if [[ -f "$PACKAGE" && -f "$YARN" ]]; then
             echo "$PACKAGE and $YARN files exist."
             synp --source-file yarn.lock
         fi
         git add .;
-        git commit -m "$1";
+        git commit -m "$str";
         git push;
         git show
     fi
